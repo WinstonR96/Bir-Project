72e0c846acf3bf6ae855c1138d3c33ffdffc272c
Winston Jr Rodriguez Stand <wijurost@gmail.com>
2019-06-07 22:26:08 -0500

    Quitando codigo que no se utiliza

--- src/index.js
+++ src/index.js
@@ -13,55 +13,13 @@ var app = express()
 
 
 
-mongoose.connect(process.env.DATABASECONECTION, { useNewUrlParser: true })
-  .then(function () {
+mongoose.connect(process.env.DATABASECONECTION, { useNewUrlParser: true, useFindAndModify:false })
+  .then( () => {
     console.log("BD Conectado")
-    // const teacherObj = {
-    //   _id: new mongoose.Types.ObjectId(),
-    //   nombre: "Winston",
-    //   apellido: "Junior",
-    //   salario:"100",
-    //   area:"Software"
-    // }
-
-    // const docente = new Teacher(teacherObj)
-    // docente.save().then(()=> console.log("Se guardo")).catch((err)=>console.error(err))
-    // const studentObj = {
-    //   _id:new mongoose.Types.ObjectId(),
-    //   nombre: "Walter",
-    //   apellido:"Samuel"
-    // }
-
-    // const estudiante = new Student(studentObj)
-    // estudiante.save().then(()=> console.log("Se guardo")).catch((err)=>console.error(err))
-    // const courseObj = {
-    //   _id:new mongoose.Types.ObjectId(),
-    //   student: studentObj._id,
-    //   teacher: teacherObj._id
-    // }
-    // const curso = new Course(courseObj)
-    // curso.save().then(()=> console.log("Se guardo")).catch((err)=>console.error(err))
-
-    //FILTRANDO ESTUDIANTE POR CURSO
-    const listaCurso = Course.findById("5cfab14e315e8596fc7f93fb")
-      .then((listaCurso) => {
-        console.log(listaCurso.student)
-        const estudiante = Student.findById(listaCurso.student)
-          .then((estudiante) => {
-            console.log(estudiante)
-          })
-          .catch((err) => console.error(err))
-      })
-      .catch((err) => console.error(err))
-
-
   })
   .catch(err => console.log(err))
 
-  console.log("Filtrando")
-  const filtrando = Course.find({student:{_id:"5cfab14e315e8596fc7f93fa"}})
-  .then((filtrando) => console.log(filtrando))
-  .catch((err) => console.error(err))
+  
 
 
 
